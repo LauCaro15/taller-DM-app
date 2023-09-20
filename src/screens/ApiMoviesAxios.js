c1bc3c24e423d6571939d7c94de8df4ce576fd0d
Natalia Pineda Serna <natalia.pinedas@autonoma.edu.co>
2023-09-20 00:00:59 -0500

    Implement cards for list items

--- src/screens/ApiMoviesAxios.js
+++ src/screens/ApiMoviesAxios.js
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react'
 import { Image, Text, View } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Surface } from '@react-native-material/core';
+import gs from "../Styles";
 
 export const ApiMoviesAxios = () => {
     const [movies, setMovies]= useState([]);
@@ -12,7 +14,7 @@ export const ApiMoviesAxios = () => {
             .get("https://taller-dm-production.up.railway.app/api/v1/movies/cast")
             .then((response) =>{
                 setMovies(response.data);
-                console.log(response.data);
+                //console.log(response.data);
             })
             .catch((error) =>  {
                 console.error("Error al obtener los datos:", error);
@@ -23,12 +25,16 @@ export const ApiMoviesAxios = () => {
         <SafeAreaView>
             <FlatList
             data = {movies}
+            columnWrapperStyle={{ flexWrap: 'wrap', flex: 1 , justifyContent: 'center'}}
+            numColumns={25}
+            style = {{ margin: 5 }}
             keyExtractor = {( item ) => item.name}
             renderItem = {({ item }) => (
-                <View>
-                    <Image source = {{ uri: item.poster }} style = {{ width: 125 , height:200 }}></Image>
-                    <Text>{ item.name }</Text>
-                </View>
+                <Surface elevation={4}
+                style={[gs.card]}>
+                    <Text style={ [ gs.cardText , gs.cardTitle ] }>{ item.name } </Text>
+                    <Image source = {{ uri: item.poster }} style = {{ width: 125 , height:200 }}/>
+                </Surface>
             )}
             ></FlatList>
         </SafeAreaView>
